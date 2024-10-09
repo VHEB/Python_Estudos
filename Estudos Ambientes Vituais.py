d8813daa960d340d08f92a75dff6855056137d97
VHEB <vheb0316@gmail.com>
2024-10-09 11:17:55 -0300

    Introdução a Ambiente Virtual

---
+++ Estudos Ambientes Vituais.py
@@ -0,0 +1,17 @@
+#Ambiente Virtual
+"""
+Ambiente Virtual carrega toda a sua instalação do Python
+em um diretório isolado, permitindo que você trabalhe em um projeto
+sem afetar o ambiente global do Python.
+"""
+#pip install virtualenv
+
+#virtualenv venv - cria um ambiente virtual
+
+#No terminal, digite:
+"""
+python -v -> Versão do Python
+python -m venv nome_do_ambiente -> Cria um ambiente virtual
+cd nome_do_ambiente/Scripts -> Entra no diretório do ambiente virtual
+.venv/Scripts/activate -> Ativa o ambiente virtual
+"""
